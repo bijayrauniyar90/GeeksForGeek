bf23b8acb752fcafcdbc3b32e9fbc3a472e5210d
Bijay Prasad Shah Rauniyar <105978662+bijayrauniyar90@users.noreply.github.com>
2024-09-03 12:25:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int minOperations(string str1, string str2) {
+        int m = str1.size(), n = str2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Build the DP array for LCS length calculation
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1[i - 1] == str2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        // Minimum operations to make both strings equal
+        int lcsLength = dp[m][n];
+        return (m - lcsLength) + (n - lcsLength);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
